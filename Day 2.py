dd2bfe3cdc0bbd896627273cbf5b94aa9660058c
Garett Foster <fostgare@oregonstate.edu>
2025-12-02 14:34:00 -0600

    Day 2 Part 1: solved

--- Day 2.py
+++ Day 2.py
@@ -13,6 +13,13 @@ for line in puzzle_input:
     for i in range(len(line)):
         id_range = line[i]
         first_id, last_id = str(id_range).split("-")
+        for prod_id in range(int(first_id), int(last_id) + 1):
+            test_id = str(prod_id)
+            if len(test_id) % 2 == 0:
+                slicer = int(len(test_id)/2)
+                if test_id[:(slicer)] == test_id[slicer:]:
+                    invalid_sum += prod_id
 
+print(invalid_sum)
 
 file.close()
