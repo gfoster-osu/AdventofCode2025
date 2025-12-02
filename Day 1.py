88d22f5bc857fe87c53837dad876cc78f282530d
Garett Foster <fostgare@oregonstate.edu>
2025-12-01 21:20:47 -0600

    Part 1 solved Part 2 solved (but it's hardly elegant)

--- Day 1.py
+++ Day 1.py
@@ -25,28 +25,31 @@ for line in instructions:
     if location == 0:
         counter += 1
 
-print(f'Part 1 Counter: {counter}\n')
+print(f'Part 1 Counter: {counter}')
 
 # Part 2:
 instructions = open('day1_data.txt')
 
 counter = 0
 location = 50
+
 for line in instructions:
     code = line.strip()
     direction = code[0]
     distance = int(code[1:])
 
-    if direction == 'L':
-        location = location - distance
+    if direction == "L":
+        for i in range(distance):
+            location -= 1
+            if location == 0:
+                counter += 1
+            elif location == -1:
+                location = 99
     else:
-        location = location + distance
+        for i in range(distance):
+            location += 1
+            if location == 100:
+                location = 0
+                counter += 1
 
-    while location < 0:
-        counter += 1
-        location = 100 + location
-    while location > 99:
-        counter += 1
-        location = location - 100
-
-print(f'Part 2 Counter: {counter}\n')
+print(f'Part 2 Counter: {counter}')
