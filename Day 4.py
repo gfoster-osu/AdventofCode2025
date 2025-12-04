8b9540c49946787cc61b74251de62da903de2476
Garett Foster <fostgare@oregonstate.edu>
2025-12-04 16:32:24 -0600

    Day 4 Part 1: solved Day 4 Part 2: solved by intentionally implementing the bug that I originally fought in part 1. nice.

--- Day 4.py
+++ Day 4.py
@@ -3,6 +3,32 @@
 # Date: 2025-12-04
 
 # Part 1:
+# puzzle_input = open('day4_data.txt')
+#
+# matrix = list()
+#
+# for line in puzzle_input:
+#     matrix.append(list(line.strip()))
+#
+# open_counter = 0
+#
+# for i in range(len(matrix)):
+#     for j in range(len(matrix[i])):
+#         if matrix[i][j] == '@':
+#             adjacent_counter = 0
+#             for k in range(i - 1, i + 2):
+#                 if k >= 0 and k < len(matrix):
+#                     for l in range(j - 1, j + 2):
+#                         if l >= 0 and l < len(matrix[i]):
+#                             if matrix[k][l] == '@':
+#                                 adjacent_counter += 1
+#             if adjacent_counter < 5:
+#                 open_counter += 1
+#
+# print(open_counter)
+# puzzle_input.close()
+
+# Part 2:
 puzzle_input = open('day4_data.txt')
 
 matrix = list()
@@ -11,19 +37,24 @@ for line in puzzle_input:
     matrix.append(list(line.strip()))
 
 open_counter = 0
+change_counter = -1
 
-for i in range(len(matrix)):
-    for j in range(len(matrix[i])):
-        if matrix[i][j] == '@':
-            adjacent_counter = 0
-            for k in range(i - 1, i + 2):
-                if k >= 0 and k < len(matrix):
-                    for l in range(j - 1, j + 2):
-                        if l >= 0 and l < len(matrix[i]):
-                            if matrix[k][l] == '@':
-                                adjacent_counter += 1
-            if adjacent_counter < 5:
-                open_counter += 1
+while change_counter != 0:
+    change_counter = 0
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            if matrix[i][j] == '@':
+                adjacent_counter = 0
+                for k in range(i - 1, i + 2):
+                    if k >= 0 and k < len(matrix):
+                        for l in range(j - 1, j + 2):
+                            if l >= 0 and l < len(matrix[i]):
+                                if matrix[k][l] == '@':
+                                    adjacent_counter += 1
+                if adjacent_counter < 5:
+                    open_counter += 1
+                    matrix[i][j] = 'X'
+                    change_counter += 1
 
 print(open_counter)
 puzzle_input.close()
