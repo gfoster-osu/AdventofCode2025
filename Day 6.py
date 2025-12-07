20f460efba0ecd0f4d0b9a3241b7aabdcd6d4215
Garett Foster <fostgare@oregonstate.edu>
2025-12-06 22:29:56 -0600

    Day 6 Part 1: solved Day 6 Part 2: solved

--- Day 6.py
+++ Day 6.py
@@ -16,7 +16,6 @@ for line in puzzle_input:
         elif raw_row[i] != "":
             row.append(int(raw_row[i]))
     raw_matrix.append(row)
-    # print(raw_matrix)
 
 problems = list()
 for i in range(len(raw_matrix[0])):
@@ -40,6 +39,57 @@ for row in problems:
 
     grand_total += solution
 
-# print(problems)
 print(grand_total)
+puzzle_input.close()
+
+# Part 2
+puzzle_input = open('day6_data.txt')
+
+raw_matrix = list()
+
+for line in puzzle_input:
+    cr = list()
+    for i in line:
+        cr.append(i)
+    raw_matrix.append(cr)
+
+transposed = reversed([list(row) for row in zip(*raw_matrix)])
+
+grand_total = 0
+
+operands = list()
+operator = None
+solution = 0
+
+for line in transposed:
+    digits = list()
+    for i in range(len(line)):
+        if line[i] == "+":
+            number = int(''.join(map(str, digits)))
+            operands.append(number)
+            for x in operands:
+                solution += x
+            grand_total += solution
+            solution = 0
+            operands = list()
+            digits = list()
+        elif line[i] == "*":
+            number = int(''.join(map(str, digits)))
+            operands.append(number)
+            solution = 1
+            for x in operands:
+                solution *= x
+            grand_total += solution
+            solution = 0
+            operands = list()
+            digits = list()
+        elif i == len(line) - 1:
+            if len(digits) > 0:
+                number = int(''.join(map(str, digits)))
+                operands.append(number)
+        elif line[i].isdigit():
+            digits.append(int(line[i]))
+
+print(grand_total)
+
 puzzle_input.close()
